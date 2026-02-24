f7feeaeaac30e7283fbab94a6e92cfef16573405
Sergio Moreno Adame <sergio.moreno.adame@gmail.com>
2026-02-24 09:10:54 +0100

    Update Coordenada.java

--- TresEnRayaSergio_Juande_JoseAntonio/src/Modelo/Coordenada.java
+++ TresEnRayaSergio_Juande_JoseAntonio/src/Modelo/Coordenada.java
@@ -2,4 +2,33 @@ package Modelo;
 
 public class Coordenada {
 
+	private int x, y;
+
+	public Coordenada(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public boolean isContigua(Coordenada antigua) {
+		int x = this.x - antigua.getX(), y = this.y - antigua.getY();
+		if (x > -2 && x < 2 && y > -2 && y < 2)
+			return true;
+		return false;
+
+	}
+	
+	
+	
+	public int getX() {
+		return x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	
 }
